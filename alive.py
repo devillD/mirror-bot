18cc43bbd246720a6fc7a48a409d2ee6b65615c4
Devill Don <53294521+devillD@users.noreply.github.com>
2022-03-09 15:30:42 +0600

    Update alive.py

--- alive.py
+++ alive.py
@@ -14,7 +14,7 @@ PORT = environ.get('PORT', None)
 if PORT is not None and BASE_URL is not None:
     while True:
         try:
-            rget(BASE_URL).status_code
+            rget('https://ping.up.railway.app/?link='BASE_URL).status_code
             sleep(600)
         except Exception as e:
             logerror(f"alive.py: {e}")
